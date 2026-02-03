d7bb84423a2a358907cba190b4b2b3d8c2770ea9
Manos <manosnikitakis3@gmail.com>
2026-02-03 13:50:46 +0200

    Add home page

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { motion } from "framer-motion"
+
+
+function App() {
+
+  return (
+    <div className='bg-primary h-screen w-full items-center text-text-primary flex flex-col container mx-auto px-4 sm:px-6 lg:px-8 pt-50 space-y-13 text-center'>
+     <h1 className="heading-style-h1">MANOS NIKITAKIS</h1>
+     <h3 className="heading-6">A recent university graduate showcasing projects across front-end and back-end development, with a focus on building practical and well-structured digital experiences.</h3>
+     <a
+              href="#projects"
+              className="cursor-pointer bg-[#222] text-white py-2 px-4 rounded-sm mt-40"
+    
+            >
+              click
+            </a>
+             
+    </div>
+  )
+}
+
+export default App
