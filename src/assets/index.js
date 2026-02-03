7aa1912e966e7e232510731790f7084705209304
Manos <manosnikitakis3@gmail.com>
2026-02-03 13:46:38 +0200

    Add logos and videos

---
+++ src/assets/index.js
@@ -0,0 +1,22 @@
+import yugioh from './yugioh.png'
+import project1 from './project1.mp4'
+import project2 from './project2.mp4'
+import united_kingdom_logo from './united_kingdom_logo.png'
+import france_logo from './france_logo.png'
+import unipi_logo from './unipi_logo.png'
+import github_logo from './github_logo.png'
+import linkedin_logo from './linkedin_logo.png'
+import email_logo from './email_logo.png'
+
+
+export {
+    yugioh,
+    project1,
+    project2,
+    united_kingdom_logo,
+    france_logo,
+    unipi_logo,
+    github_logo,
+    linkedin_logo,
+    email_logo
+}
